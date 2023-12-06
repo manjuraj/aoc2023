1fd94fb41ba44bd14064b802ebbf02600a44a8fc
Manju Rajashekhar <manj@cs.stanford.edu>
2023-12-05 18:10:46 -0800

    Day 6 scaffolding

---
+++ rs/src/day06.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+
+pub fn part1_and_part2() -> Result<()> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_with_sample() -> Result<()> {
+        Ok(())
+    }
+}
